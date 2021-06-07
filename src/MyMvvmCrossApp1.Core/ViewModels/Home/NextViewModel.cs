0de702a127c93b3d5a247024c3f826250a709756
Stefanos Papamichail <spapamichail@indeavor.com>
2021-06-07 12:12:49 +0300

    1.1.2

---
+++ src/MyMvvmCrossApp1.Core/ViewModels/Home/NextViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyMvvmCrossApp1.Core.ViewModels.Home
+{
+    public class NextViewModel:BaseViewModel
+    {
+    }
+}
